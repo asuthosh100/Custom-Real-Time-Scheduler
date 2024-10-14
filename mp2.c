2743c05557b49a4ee42a6152d0eb51e1fe314be6
asuthoshaa69 <aa69@illinois.edu>
2024-10-14 18:05:49 -0500

    Check ProcFs

--- mp2.c
+++ mp2.c
@@ -2,39 +2,86 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/init.h>
+
 #include "mp2_given.h"
 
 // !!!!!!!!!!!!! IMPORTANT !!!!!!!!!!!!!
 // Please put your name and email here
-MODULE_AUTHOR("Tianyin Xu <tyxu@illinois.edu>");
+MODULE_AUTHOR("Asuthosh Anandaram <aa69@illinois.edu>");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("CS-423 MP2");
 
+static struct proc_dir_entry *proc_dir, *proc_entry; 
+
+
 #define DEBUG 1
+//------------------------------------------------------------------
+static ssize_t myread(struct file *file, char __user *ubuf, size_t count, loff_t *ppos) 
+{
+	printk( KERN_DEBUG "read handler\n");
+	return 0;
+}
+
+static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos) 
+{
+	printk( KERN_DEBUG "write handler\n");
+	return 0;
+	
+}
+
+
+static const struct proc_ops mp1_ops = 
+{
+	.proc_open = simple_open,
+	.proc_read = myread,
+	.proc_write = mywrite,
+};
+
 
 // mp2_init - Called when module is loaded
-int __init mp2_init(void)
+int __init rts_init(void)
 {
 #ifdef DEBUG
-	printk(KERN_ALERT "MP2 MODULE LOADING\n");
+	printk(KERN_ALERT "RTS MODULE LOADING\n");
 #endif
 	// Insert your code here ...
 
-	printk(KERN_ALERT "MP2 MODULE LOADED\n");
+	proc_dir = proc_mkdir("mp2", NULL);
+	printk(KERN_ALERT "mp1 created....\n"); 
+
+	proc_entry = proc_create("status", 0666, proc_dir &mp1_ops);
+
+	if (!proc_entry) {
+		printk(KERN_ALERT "status creation failed....\n");
+		return -ENOMEM;
+	}
+	printk(KERN_ALERT "status created....\n");
+
+
+	printk(KERN_ALERT "RTS MODULE LOADED\n");
 	return 0;
 }
 
 // mp2_exit - Called when module is unloaded
-void __exit mp2_exit(void)
+void __exit rts_exit(void)
 {
 #ifdef DEBUG
-	printk(KERN_ALERT "MP2 MODULE UNLOADING\n");
+	printk(KERN_ALERT "RTS MODULE UNLOADING\n");
 #endif
 	// Insert your code here ...
 
-	printk(KERN_ALERT "MP2 MODULE UNLOADED\n");
+	remove_proc_entry("status", proc_dir);
+	printk(KERN_WARNING "status removed....\n");
+
+	// Remove the directory within the proc filesystem
+	remove_proc_entry("mp2", NULL);
+	printk(KERN_WARNING "mp2 removed...\n");
+
+	printk(KERN_ALERT "RTS MODULE UNLOADED\n");
 }
 
 // Register init and exit funtions
-module_init(mp2_init);
-module_exit(mp2_exit);
+module_init(rts_init);
+module_exit(rts_exit);
