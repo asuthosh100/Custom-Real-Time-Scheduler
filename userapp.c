bbb12f7101aa894f1e49e3b6152caec8f3756e46
asuthoshaa69 <aa69@illinois.edu>
2024-10-16 18:20:51 -0500

    step2

--- userapp.c
+++ userapp.c
@@ -6,14 +6,16 @@
 #include <stdlib.h>
 
 #define FILE_PATH "/proc/mp2/status"
+#define SIZE 2048
 
 int main(int argc, char *argv[])
 {
     pid_t pid = getpid(); 
     int fd; 
+    char buffer[SIZE];
 
-    unsigned long period = atoi(argv[1]); 
-    unsigned long processing_time = atoi(argv[2]); 
+    unsigned int period = atoi(argv[1]); 
+    unsigned int processing_time = atoi(argv[2]); 
 //-------------REGISTER-------------------------------
     FILE *file = fopen(FILE_PATH, "w"); 
     
@@ -22,8 +24,10 @@ int main(int argc, char *argv[])
         exit(1);
     }
     
-    fprintf(file, "R,%d,%lu,%lu\n", pid, period, processing_time);
+    //fprintf(file, "R,%d,%u,%u\n", pid, period, processing_time);
 
+    snprintf(buffer, sizeof(buffer), "R,%d,%lu,%lu\n", pid, period, processing_time);
+    fwrite(buffer, sizeof(char), strlen(buffer), file);
    // printk("Sent to kernel "R,%d,%lu,%lu\n", pid, period, processing_time);
     printf("Sent to kernel:R,%d,%lu,%lu\n", pid, period, processing_time);
     fclose(file); 
